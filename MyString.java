e5f3d50b084bc4ac3ec8fbbbeb8c88c3a5e4990e
Guy <lichtguy@gmail.com>
2024-12-05 17:25:07 +0200

    MYSTRING MTA1

--- MyString.java
+++ MyString.java
@@ -18,13 +18,38 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String lowerCase = "";
+        for (int i = 0; i < str.length(); i++) {
+            if ((str.charAt(i) >= 65) && (str.charAt(i) <= 90)) {
+                lowerCase += (char) (str.charAt(i) + 32);
+            } else {
+                lowerCase += str.charAt(i);
+            }
+        }
+        return lowerCase;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        int sw = 0;
+        if (str2.length() > str1.length()) {
+            return false;
+        } else if ((str1.length() == 0) || (str2.length() == 0)) {
+            return true;
+        }
+        for (int i = 0; i <= (str1.length() - str2.length()); i++) {
+            if (str1.charAt(i) == str2.charAt(0)) {
+                for (int j = 0; j < str2.length(); j++) {
+                    if (str1.charAt(i + j) != str2.charAt(j)) {
+                        sw = 1;
+                        break;
+                    }
+                }
+                if (sw == 0) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
